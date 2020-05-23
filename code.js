77ced83d2b2fbdd7b3c69f2973f6da9bc48f9042
mohmed <mohmedamged9@gmail.com>
2020-05-23 04:17:38 +0200

    added the edit

--- code.js
+++ code.js
@@ -1,5 +1,11 @@
 "use strict"
 
+let toDo = {
+    submit: true,
+    edit: false,
+    editItem: ""
+}
+
 let input = document.getElementById("in-js");
 
 let mode = document.getElementById("mode");
@@ -8,22 +14,43 @@ let list = document.getElementById('list');
 
 
 list.addEventListener("click", (e) => {
+    let p = e.target.parentElement.parentElement;
     if (e.target.classList.contains("delete")) {
         console.log(e.parentElement);
-        let p = e.target.parentElement.parentElement;
+
         list.removeChild(p);
+    }
+    else if (e.target.classList.contains("edit")) {
+        mode.innerText = "Edit";
+        input.value = p.firstElementChild.innerText;
+        toDo.editItem = p.firstElementChild;
+        toDo.edit = true;
+        toDo.submit = false;
     }
 })
 
 mode.addEventListener("click", () => {
     if (input.value !== "") {
-        list.innerHTML += `<li class="list__item">
+        if (toDo.edit) {
+            console.log(toDo.editItem);
+            console.log(input.value);
+            toDo.editItem.innerText = input.value;
+            toDo.editItem = "";
+            toDo.edit = false;
+            toDo.submit = true;
+            mode.innerText = "Submite";
+            console.log(toDo)
+        }
+        else {
+            list.innerHTML += `<li class="list__item">
                     <p>${input.value}</p>
                     <div class="icons">
                         <i class="fas fa-edit edit"></i>
                         <i class="fas fa-trash-alt delete"></i>
                     </div>
                 </li>`;
+
+        }
         input.value = "";
     }
 
