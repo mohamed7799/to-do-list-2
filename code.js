99f9e1f7270172968ae22c40324ab1eb44f7d75c
mohmed <mohmedamged9@gmail.com>
2020-05-23 15:59:17 +0200

    added the search function and refactored the code

--- code.js
+++ code.js
@@ -7,18 +7,45 @@ let toDo = {
     setValues: function (sub, ed, it) {
         this.submit = sub;
         this.edit = ed;
-        thiss.editItem = it;
+        this.editItem = it;
     }
 }
 
-let input = document.getElementById("in-js");
+
+let inputSub = document.getElementById("sub-in");
+
+let inputSearch = document.getElementById("search-in");
 
 let mode = document.getElementById("mode");
 
 let list = document.getElementById('list');
 
-
 //event listeners
+
+
+
+inputSearch.addEventListener("keyup", () => {
+    let tasks = [...list.getElementsByTagName("p")];
+    let term = inputSearch.value;
+    tasks.forEach((ele) => {
+        let title = ele.textContent;
+
+        if (title.toLowerCase().indexOf(term) != -1) {
+            let temp = ele.parentElement;
+
+            temp.style.display = "flex";
+        }
+        else {
+            let temp = ele.parentElement;
+
+            temp.style.display = "none";
+        }
+    })
+})
+
+
+
+
 
 list.addEventListener("click", (e) => {
     let p = e.target.parentElement.parentElement;
@@ -27,21 +54,21 @@ list.addEventListener("click", (e) => {
     }
     else if (e.target.classList.contains("edit")) {
         mode.innerText = "Edit";
-        input.value = p.firstElementChild.innerText;
+        inputSub.value = p.firstElementChild.innerText;
         toDo.setValues(false, true, p.firstElementChild);
     }
 })
 
 mode.addEventListener("click", () => {
-    if (input.value !== "") {
+    if (inputSub.value !== "") {
         if (toDo.edit) {
-            toDo.editItem.innerText = input.value;
+            toDo.editItem.innerText = inputSub.value;
             toDo.setValues(true, false, "");
             mode.innerText = "Submite";
         }
         else {
             list.innerHTML += `<li class="list__item">
-                    <p>${input.value}</p>
+                    <p>${inputSub.value}</p>
                     <div class="icons">
                         <i class="fas fa-edit edit"></i>
                         <i class="fas fa-trash-alt delete"></i>
@@ -49,7 +76,7 @@ mode.addEventListener("click", () => {
                 </li>`;
 
         }
-        input.value = "";
+        inputSub.value = "";
     }
 
 
