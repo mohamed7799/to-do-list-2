17f6acdad09b2c435f193ac231b8e7996ef7171a
mohmed <mohmedamged9@gmail.com>
2020-05-29 19:21:44 +0200

    add firebase firestore to the project

--- code.js
+++ code.js
@@ -11,6 +11,7 @@ let toDo = {
     }
 }
 
+let db = firebase.firestore();
 
 let inputSub = document.getElementById("sub-in");
 
@@ -20,9 +21,46 @@ let mode = document.getElementById("mode");
 
 let list = document.getElementById('list');
 
+
+//fun
+
+let renderItem = (item, value, id) => {
+    item.innerHTML += `<li class="list__item">
+                    <p id=${id}>${value}</p>
+                    <div class="icons">
+                        <i class="fas fa-edit edit"></i>
+                        <i  class="fas fa-trash-alt delete"></i>
+                    </div>
+                </li>`;
+}
+
+
+let getDB = () => {
+    db.collection("tasks").get().then((snap) => {
+        snap.docs.forEach((doc) => {
+            renderItem(list, doc.data().task, doc.id);
+        })
+    })
+
+}
+
+
+let addToDB = (task) => {
+
+    db.collection("tasks").add({
+        task: task
+    })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+            return docRef.id;
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+}
+
+
 //event listeners
-
-
 
 inputSearch.addEventListener("keyup", () => {
     let tasks = [...list.getElementsByTagName("p")];
@@ -45,17 +83,17 @@ inputSearch.addEventListener("keyup", () => {
 
 
 
-
-
 list.addEventListener("click", (e) => {
-    let p = e.target.parentElement.parentElement;
+    let p = e.target.parentElement.previousElementSibling;
+    let id = p.id;
     if (e.target.classList.contains("delete")) {
+        db.collection("tasks").doc(id).delete();
         list.removeChild(p);
     }
     else if (e.target.classList.contains("edit")) {
         mode.innerText = "Edit";
-        inputSub.value = p.firstElementChild.innerText;
-        toDo.setValues(false, true, p.firstElementChild);
+        inputSub.value = p.innerText;
+        toDo.setValues(false, true, p);
     }
 })
 
@@ -63,24 +101,23 @@ mode.addEventListener("click", () => {
     if (inputSub.value !== "") {
         if (toDo.edit) {
             toDo.editItem.innerText = inputSub.value;
+            db.collection("tasks").doc(toDo.editItem.id).set({ task: inputSub.value });
             toDo.setValues(true, false, "");
             mode.innerText = "Submite";
         }
         else {
-            list.innerHTML += `<li class="list__item">
-                    <p>${inputSub.value}</p>
-                    <div class="icons">
-                        <i class="fas fa-edit edit"></i>
-                        <i class="fas fa-trash-alt delete"></i>
-                    </div>
-                </li>`;
-
+            let id = addToDB(inputSub.value);
+            renderItem(list, inputSub.value, id);
         }
         inputSub.value = "";
     }
-
 
     else {
         alert("Please Enter a Task")
     }
 })
+
+//main
+
+getDB();
+
